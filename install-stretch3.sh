bfe9d0604444ba9781b344aa378d5e58a6cd4edb
yokobond <koji.yokokawa@gmail.com>
2021-11-21 18:34:59 +0900

    Change dir config for latest register script

--- install-stretch3.sh
+++ install-stretch3.sh
@@ -9,18 +9,18 @@ LF=$(printf '\\\012_')
 LF=${LF%_}
 
 mkdir -p node_modules/scratch-vm/src/extensions/microbitMore
-sed -e "s/let formatMessage = .*;/let formatMessage \= require('format-message');/" microbitMore/src/block/index.js > node_modules/scratch-vm/src/extensions/microbitMore/index.js
-cp microbitMore/src/block/ble-web.js node_modules/scratch-vm/src/extensions/microbitMore/
-cp microbitMore/src/block/ble-llk.js node_modules/scratch-vm/src/extensions/microbitMore/ble.js
-cp microbitMore/src/block/serial-web.js node_modules/scratch-vm/src/extensions/microbitMore/
+sed -e "s/let formatMessage = .*;/let formatMessage \= require('format-message');/" microbitMore/src/vm/extensions/block/index.js > node_modules/scratch-vm/src/extensions/microbitMore/index.js
+cp microbitMore/src/vm/extensions/block/ble-web.js node_modules/scratch-vm/src/extensions/microbitMore/
+cp microbitMore/src/vm/extensions/block/ble-llk.js node_modules/scratch-vm/src/extensions/microbitMore/ble.js
+cp microbitMore/src/vm/extensions/block/serial-web.js node_modules/scratch-vm/src/extensions/microbitMore/
 mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
 sed -e "s|class ExtensionManager {$|builtinExtensions['microbitMore'] = () => require('../extensions/microbitMore');${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
 
 mkdir -p src/lib/libraries/extensions/microbitMore
-cp microbitMore/src/entry/entry-icon.png src/lib/libraries/extensions/microbitMore/
-cp microbitMore/src/entry/inset-icon.svg src/lib/libraries/extensions/microbitMore/
-cp microbitMore/src/entry/connection-icon.svg src/lib/libraries/extensions/microbitMore/
-cp microbitMore/src/entry/connection-small-icon.svg src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/gui/lib/libraries/extensions/entry/entry-icon.png src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/gui/lib/libraries/extensions/entry/inset-icon.svg src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/gui/lib/libraries/extensions/entry/connection-icon.svg src/lib/libraries/extensions/microbitMore/
+cp microbitMore/src/gui/lib/libraries/extensions/entry/connection-small-icon.svg src/lib/libraries/extensions/microbitMore/
 mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
 MICROBIT_MORE="\
     {${LF}\
